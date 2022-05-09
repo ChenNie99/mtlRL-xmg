9c439f652797c1287c49e9c32de205ea26b6be19
lvchenyangAI <lvchenyangai@outlook.com>
2022-05-10 00:55:49 +0800

    new bash command

--- verilog_to_xmg.sh
+++ verilog_to_xmg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-read -p "Please Enter You file:: " NAME
+read -p "Please Enter You file: " NAME
 echo "Your fileName Is: $NAME"
-ls
 yosys -p "read_verilog ${NAME}.v; synth -auto-top; clean; write_verilog ${NAME}_syn.v"
 ./converter -v ${NAME}_syn.v
 ./mig ${NAME}_syn_out.v 1 ""
+cd /home/abcRL2.0-4-24
+python testReinforce.py -i "/home/MIG_project-main/${NAME}_syn_out_opt_1.v" -n "${NAME}"
