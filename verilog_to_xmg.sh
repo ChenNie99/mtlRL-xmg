bab22e862d6c4f9333614cb7c5ef6040555eef29
lvchenyangAI <lvchenyangai@outlook.com>
2022-05-10 16:37:46 +0800

    new bash script

--- verilog_to_xmg.sh
+++ verilog_to_xmg.sh
@@ -5,3 +5,5 @@ ls
 yosys -p "read_verilog ${NAME}.v; synth -auto-top; clean; write_verilog ${NAME}_syn.v"
 ./converter -v ${NAME}_syn.v
 ./mig ${NAME}_syn_out.v 1 ""
+cd /home/abcRL2.0-4-24
+python testReinforce.py -i "/home/MIG_project-main/${NAME}_syn_out_opt_1.v" -n "${NAME}"
